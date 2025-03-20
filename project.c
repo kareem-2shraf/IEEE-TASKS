59685c602184d022c046099a012e2bc9414d44d7
kareem-2shraf <karem2ashraf@gmail.com>
2025-03-20 19:04:59 +0200

    Update project.c

--- project.c
+++ project.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct // struct to store the data of the every product
+typedef struct // struct to store the data of every product
 {
     char name [100];
     int id;
